313cd8ed68fedec39a36f4ba38a660cc4b444634
yubowen-ph <yubowen.ph@gmail.com>
2018-06-09 12:07:23 +0800

    first commit

--- train/word_hcnn.py
+++ train/word_hcnn.py
@@ -62,8 +62,8 @@ for i in range(15):
        y = train.label.values,
         epochs = 1,
         # steps_per_epoch = int(train.shape[0]/batch_size),
-        validation_split = 0.2
-        # batch_size = batch_size
+        validation_split = 0.2,
+        batch_size = batch_size,
         # validation_data = (val_seq, val_label)
     )
 
